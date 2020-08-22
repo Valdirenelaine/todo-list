a64a202faab04d1bfd19f28b4e7c198fdea70255
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-08-22 09:33:15 -0400

    corrigindo o erro da tag

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app-bar color="blue" app dark>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <toolbar-tittle>Tarefas</toolbar-tittle>
+        <v-toolbar-title>Tarefas</v-toolbar-title>
     </v-app-bar>
 </template>
